ee596fbc6b6d325ef4dd7149d3d2466ba4b52675
Joel  Bly <jtb6179@g.rit.edu>
2019-11-27 22:25:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     if programmer_hash[:dennis_ritchie][:languages] = ["Smalltalk", "LISP", "Assembly"]
+     if programmer_hash[:dennis_ritchie][:languages => "Smalltalk", "LISP", "Assembly"]
+     else 
+       
        
 
 end
