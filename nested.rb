67a4d60c51c7f17bd3cb69b52c3d1426eea753e0
Joel  Bly <jtb6179@g.rit.edu>
2019-11-27 22:23:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     if programmer_hash[:dennis_ritchie][:languages]
+     if programmer_hash[:dennis_ritchie][:languages] 
 
 end
