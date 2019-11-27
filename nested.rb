f0feabb71ae0904b0c919b2c316dd6613f4b8ef6
Joel  Bly <jtb6179@g.rit.edu>
2019-11-27 22:43:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,7 +130,7 @@ def adding_to_dennis
      }
      if programmer_hash[:dennis_ritchie][:languages][1] += 1
      else
-       programmer_hash[:dennis_ritchie][:languages => [1] = "Assembly"
+       programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
      end 
   return programmer_hash
 end
